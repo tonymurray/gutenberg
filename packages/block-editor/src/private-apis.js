d51b56bf07643d48ba4124e63954b7e75e92848d
Alex Lende <alex@lende.xyz>
2023-06-08 12:56:44 -0500

    Add a privateApis export for experimental ImageSizeControl

--- packages/block-editor/src/private-apis.js
+++ packages/block-editor/src/private-apis.js
@@ -12,6 +12,7 @@ import { useShouldContextualToolbarShow } from './utils/use-should-contextual-to
 import { cleanEmptyObject } from './hooks/utils';
 import { useBlockEditingMode } from './components/block-editing-mode';
 import BlockQuickNavigation from './components/block-quick-navigation';
+import { PrivateImageSizeControl } from './components/image-size-control';
 
 /**
  * Private @wordpress/block-editor APIs.
@@ -28,4 +29,5 @@ lock( privateApis, {
 	cleanEmptyObject,
 	useBlockEditingMode,
 	BlockQuickNavigation,
+	PrivateImageSizeControl,
 } );
